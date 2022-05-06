c9284ff4c72a08f1a59f246cf803e39ad63e7be5
Kim Hager <hager.kimberly.a@gmail.com>
2022-05-05 21:22:21 -0400

    Play

--- Project5/script.js
+++ Project5/script.js
@@ -3,10 +3,19 @@ console.log(canvas);
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 const ctx = canvas.getContext("2d");
-ctx.lineWidth = 1;
-ctx.strokeRect(0, 0, 10, 10);
-for (let i = 0; i < width * height; i++) {
-  ctx.translate(10, 10);
-  ctx.fillStyle = "red";
-  ctx.fillRect(0, 0, 10, 10);
+
+for (let i = 0; i<10; i++){
+ctx.fillStyle="red";
+ctx.beginPath();
+ctx.ellipse(100+110*i, 100+110*i, 50, 75, Math.PI / 4, 0, 2 * Math.PI);
+ctx.stroke();
+ctx.fill();
+}
+
+for (let i = 0; i<10; i++){
+ctx.fillStyle="blue";
+ctx.beginPath();
+ctx.ellipse(100+110*i, 100+110*i, 50, 75, -Math.PI / 4, 0, 2 * Math.PI);
+ctx.stroke();
+ctx.fill();
 }
