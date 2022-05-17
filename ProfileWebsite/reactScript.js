4cefeec3f95f0818134e950b455f79e4b0201b8b
Kim Hager <hager.kimberly.a@gmail.com>
2022-05-17 19:15:02 -0400

    Working on React

--- ProfileWebsite/reactScript.js
+++ ProfileWebsite/reactScript.js
@@ -1,1 +1,26 @@
-//coming soon
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// const circle = () => (
+//   <svg width="900" height="500">
+//     <circle r="100" />
+//   </svg>
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// console.log(root);
+// root.render(circle);
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+var element = React.createElement(
+  "h1",
+  { className: "greeting" },
+  "Hello, world!"
+);
+ReactDOM.render(element, document.getElementById("root"));
+
+reportWebVitals();
--- ProfileWebsite/script.js
+++ ProfileWebsite/script.js
@@ -1,74 +1,74 @@
-window.onload = () => {
-  const superSecret = "cats";
-  const selected = document.getElementById("steps-taken");
-  const allDivs = document.querySelectorAll("div");
-  allDivs.forEach((element) => {
-    if (
-      element.id == "Research" ||
-      element.id == "Inventory" ||
-      element.id == "Interview" ||
-      element.id == "Examples"
-    ) {
-      element.style.display = "none";
-    }
-  });
+// window.onload = () => {
+//   const superSecret = "cats";
+//   const selected = document.getElementById("steps-taken");
+//   const allDivs = document.querySelectorAll("div");
+//   allDivs.forEach((element) => {
+//     if (
+//       element.id == "Research" ||
+//       element.id == "Inventory" ||
+//       element.id == "Interview" ||
+//       element.id == "Examples"
+//     ) {
+//       element.style.display = "none";
+//     }
+//   });
 
-  const button = document.getElementById("submitbutton");
-  const input = document.getElementById("password");
-  const selectedLabel = document.getElementById("steps-taken-label");
+//   const button = document.getElementById("submitbutton");
+//   const input = document.getElementById("password");
+//   const selectedLabel = document.getElementById("steps-taken-label");
 
-  let access;
-  showChoices();
+//   let access;
+//   showChoices();
 
-  button.onclick = () => {
-    showChoices();
-  };
+//   button.onclick = () => {
+//     showChoices();
+//   };
 
-  let selectedValue = selected.value;
+//   let selectedValue = selected.value;
 
-  selected.onchange = () => {
-    selectedValue = selected.value;
-    allDivs.forEach((element) => {
-      if (
-        element.id == "Research" ||
-        element.id == "Inventory" ||
-        element.id == "Interview" ||
-        element.id == "Examples"
-      ) {
-        element.style.display = "none";
-      }
-    });
-    showData(selectedValue);
-  };
+//   selected.onchange = () => {
+//     selectedValue = selected.value;
+//     allDivs.forEach((element) => {
+//       if (
+//         element.id == "Research" ||
+//         element.id == "Inventory" ||
+//         element.id == "Interview" ||
+//         element.id == "Examples"
+//       ) {
+//         element.style.display = "none";
+//       }
+//     });
+//     showData(selectedValue);
+//   };
 
-  function showChoices() {
-    access = input.value == superSecret;
-    selected.style.display = access ? "inline-block" : "none";
-    selectedLabel.style.display = access ? "inline-block" : "none";
-    button.style.display = access ? "none" : "inline-block";
-    input.style.display = access ? "none" : "inline-block";
-  }
+//   function showChoices() {
+//     access = input.value == superSecret;
+//     selected.style.display = access ? "inline-block" : "none";
+//     selectedLabel.style.display = access ? "inline-block" : "none";
+//     button.style.display = access ? "none" : "inline-block";
+//     input.style.display = access ? "none" : "inline-block";
+//   }
 
-  function showData(selectedValue) {
-    switch (true) {
-      case selectedValue == "Inventory" && access:
-        const inventoryDiv = document.getElementById("Inventory");
-        inventoryDiv.style.display = "block";
-        break;
-      case selectedValue == "Interview" && access:
-        const interviewDiv = document.getElementById("Interview");
-        interviewDiv.style.display = "block";
-        break;
-      case selectedValue == "Examples" && access:
-        const examplesDiv = document.getElementById("Examples");
-        examplesDiv.style.display = "block";
-        break;
-      case selectedValue == "Research" && access:
-        const researchDiv = document.getElementById("Research");
-        researchDiv.style.display = "block";
-        break;
-      default:
-        break;
-    }
-  }
-};
+//   function showData(selectedValue) {
+//     switch (true) {
+//       case selectedValue == "Inventory" && access:
+//         const inventoryDiv = document.getElementById("Inventory");
+//         inventoryDiv.style.display = "block";
+//         break;
+//       case selectedValue == "Interview" && access:
+//         const interviewDiv = document.getElementById("Interview");
+//         interviewDiv.style.display = "block";
+//         break;
+//       case selectedValue == "Examples" && access:
+//         const examplesDiv = document.getElementById("Examples");
+//         examplesDiv.style.display = "block";
+//         break;
+//       case selectedValue == "Research" && access:
+//         const researchDiv = document.getElementById("Research");
+//         researchDiv.style.display = "block";
+//         break;
+//       default:
+//         break;
+//     }
+//   }
+// };
