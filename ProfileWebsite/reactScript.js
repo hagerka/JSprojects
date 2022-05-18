5ee120f1793bc00ee6a13d16d89a0393befd7ccd
Kim Hager <hager.kimberly.a@gmail.com>
2022-05-17 21:45:05 -0400

    React and CSS Animations

--- ProfileWebsite/reactScript.js
+++ ProfileWebsite/reactScript.js
@@ -1,26 +1,54 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
+const lightRadius = 30;
+const rectWidth = lightRadius;
+const rectHeight = 20;
+const circleCenter = 50 + lightRadius;
 
-// const circle = () => (
-//   <svg width="900" height="500">
-//     <circle r="100" />
-//   </svg>
-// );
+const lighthouse = (
+  <svg width="250px" height="300px">
+    <g>
+      <circle r={0.8 * lightRadius} cx={circleCenter} cy={circleCenter} />
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// console.log(root);
-// root.render(circle);
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-
-var element = React.createElement(
-  "h1",
-  { className: "greeting" },
-  "Hello, world!"
+      <circle
+        r={lightRadius}
+        cx={circleCenter}
+        cy={circleCenter}
+        fill="white"
+        stroke="black"
+      />
+      <rect
+        width={rectWidth}
+        height={rectHeight}
+        x={circleCenter - rectWidth / 2}
+        y={circleCenter + rectHeight}
+        fill="white"
+        stroke="black"
+      />
+      <rect
+        width={1.05 * rectWidth}
+        height={rectHeight}
+        x={circleCenter - (1.05 * rectWidth) / 2}
+        y={circleCenter + 2 * rectHeight}
+        fill="red"
+        stroke="black"
+      />
+      <rect
+        width={1.1 * rectWidth}
+        height={rectHeight}
+        x={circleCenter - (1.1 * rectWidth) / 2}
+        y={circleCenter + 3 * rectHeight}
+        fill="white"
+        stroke="black"
+      />
+      <rect
+        width={1.15 * rectWidth}
+        height={rectHeight}
+        x={circleCenter - (1.15 * rectWidth) / 2}
+        y={circleCenter + 4 * rectHeight}
+        fill="red"
+        stroke="black"
+      />
+    </g>
+  </svg>
 );
-ReactDOM.render(element, document.getElementById("root"));
 
-reportWebVitals();
+ReactDOM.render(lighthouse, document.getElementById("root"));
